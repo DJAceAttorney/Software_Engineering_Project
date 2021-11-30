a99f6be7b370cb2c9dc064b56bdbace64fea22f6
MewZeroBox <neiljingbolima@gmail.com>
2021-11-29 21:14:24 -0500

    started adding visual layout for button.

--- workoutApp/navigation/screens/workouts.js
+++ workoutApp/navigation/screens/workouts.js
@@ -1,20 +1,29 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import WorkoutButton from '../components/button';
 
 export default function workout({ navigation }) {
     return (
         <View style={styles.container}>
-
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{width: "80%"}}>
+            <Text
+                style={styles.title}>Your Available Workouts</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <WorkoutButton/>
+                <WorkoutButton/>
+                <WorkoutButton/>
+                <WorkoutButton/>
+                <WorkoutButton/>
+            </View>
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text
                     onPress={() => alert('This is the "Workout" screen.')}
-                    style={{ color: 'white', fontSize: 26, fontWeight: 'bold' }}>Workout Screen</Text>
+                    style={{ color: 'white', fontSize: 26, fontWeight: 'bold' }}>Not Workout Screen</Text>
             </View>
-
         </View>
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -23,4 +32,18 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    buttonContainer: {
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "space-between"
+    },
+    title: {
+        color: 'white', 
+        marginVertical: 30,
+        width: "100%",
+        fontSize: 24, 
+        fontWeight: 'bold' ,
+        textAlign: "left",
+    }
 });
