ff2058e0fe0aa768fc60637e21ba6bde6a2e35d6
muktar-gif <84489366+muktar-gif@users.noreply.github.com>
2021-11-29 20:34:00 -0500

    Changed workout database structure, workouts table takes in a name and list of exercise objects

--- workoutApp/navigation/screens/addWorkouts.js
+++ workoutApp/navigation/screens/addWorkouts.js
@@ -1,83 +1,139 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { db } from '../../firebase-config';
 import { collection, getDocs, addDoc } from 'firebase/firestore';
-import { TextInput, Button } from 'react-native';
 
 export default function addWorkouts({ navigation }) {
 
-    
-    //var for getting data
+    //var for getting workout name
     const [newName, setNewName] = useState("");
-    const [newExercise, setNewExercise] = useState("");
-    const [newReps, setNewReps] = useState("");
-    const [newSets, setNewSets] = useState("");
-    const [newWeights, setNewWeights] = useState("");
+
+    //exercise object, includes name, reps, sets, weight
+    const [newExercise, setNewExercise] = useState({name: '', reps: 0, sets: 0, weight: 0});
+
+    //const [newReps, setNewReps] = useState([]);
+    //const [newSets, setNewSets] = useState([]);
+    //const [newWeights, setNewWeights] = useState([]);
 
     //db reference to workouts table
     const workoutsCollectionRef = collection(db, "workouts");
+
+    //workouts holds db []
+    const [workouts, setWorkouts] = useState([]);
+
+
+    useEffect (() => {
+
+        //getDoc sets workouts from the database
+        const getWorkouts = async () => {
+
+            const data = await getDocs(workoutsCollectionRef);
+            setWorkouts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
+
+        }
+
+        getWorkouts()
+
+    })
     
     const createWorkout = async () => {
 
-        await addDoc(workoutsCollectionRef, {name: newName, exercises: newExercise, reps: newReps, sets: newSets, weight: newWeights})
+        //adds workout with a name and a list with an exercise object (may start with an empty list and add objects to list separately with function addWorkoutExer())
+        await addDoc(workoutsCollectionRef, {name: newName, exercises: [newExercise]})
 
     }
 
+    /*
+
+    //function to delete workout
+    const deleteWorkout = async () => {
+
+
+
+    }
+
+    //function to update/add an exercise object to list of exercises given a workout
+    const addWorkoutExercise = async () => {
+
+
+
+    }
+    
+    //function to update/delete an exercise object from list of exercises
+    const deleteWorkoutExercise = async () => {
+
+
+
+    }
+    
+    */
+
     return (
-        <View style={styles.container}>
 
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text
-                    onPress={() => navigation.navigate('Workouts')}
-                    style={{ color: 'white', fontSize: 26, fontWeight: 'bold' }}>Add workout screen</Text>
-            
+        <TouchableWithoutFeedback onPress = { () => {
 
-            
-                <TextInput
-                    onChangeText = {newName => 
-                        setNewName(newName)
-                    }
-                    value = {newName}
-                    placeholder = "Enter a Workout Name"
-                    />
+            Keyboard.dismiss();
 
-                <TextInput
-                    onChangeText = {newExercise => 
-                        setNewExercise(newExercise)
-                    }
-                    value = {newExercise}
-                    placeholder = "Enter a exercise"
-                    />
+        }}>
+            <View style={styles.container}>
 
-                <TextInput
-                    onChangeText = {newReps => 
-                        setNewReps(newReps)
-                    }
-                    value = {newReps}
-                    placeholder = "Enter reps"
-                    />
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-                <TextInput
-                    onChangeText = {newSets => 
-                        setNewSets(newSets)
-                    }
-                    value = {newSets}
-                    placeholder = "Enter sets"
-                    />
-
-                <TextInput
-                    onChangeText = {newWeights => 
-                        setNewWeights(newWeights)
-                    }
-                    value = {newWeights}
-                    placeholder = "Enter a weight (lbs)"
-                    />
+                    <Text
+                        onPress={() => navigation.navigate('Workouts')}
+                        style={{ color: 'white', fontSize: 26, fontWeight: 'bold' }}>Add workout screen</Text>
                 
-                <Button title = "Add a workout" onPress = {createWorkout}/>
+                    <TextInput
+                        onChangeText = {newName => 
+                            setNewName(newName)
+                        }
+                        value = {newName}
+                        placeholder = "Enter a Workout Name"
+                        />
+
+                    <TextInput
+                        onChangeText = {x => 
+                            setNewExercise({...newExercise, name: x})
+                        }
+                        value = {newExercise.name}
+                        placeholder = "Enter a exercise"
+                        />
+
+                    
+                        
+                    <TextInput
+                        onChangeText = {x => 
+                            setNewExercise({...newExercise, reps: parseInt(x)})
+                        }
+                        keyboardType='numeric'
+                        placeholder = "Enter reps"
+                        />
+
+                    <TextInput
+                        onChangeText = {x => 
+                            setNewExercise({...newExercise, sets: parseInt(x)})
+                        }
+                        keyboardType='numeric'
+                        placeholder = "Enter sets"
+                        />
+
+                    <TextInput
+                        onChangeText = {x => 
+                            setNewExercise({...newExercise, weight: parseInt(x)})
+                        }
+                        keyboardType='numeric'
+                        placeholder = "Enter a weight (lbs)"
+                        />
+                        
                 
+                    
+                    <Button title = "Add a workout" onPress = {createWorkout}/>
+                    {/*<Button title = "Add to workout" onPress = {updateExercise()}/>*/}
+
+                </View>
             </View>
-        </View>
+        </TouchableWithoutFeedback>
     );
 }
 
