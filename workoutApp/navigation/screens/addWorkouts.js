dd5a246a3451c27161ebd916b919548e0d905256
MewZeroBox <neiljingbolima@gmail.com>
2021-11-30 20:36:58 -0500

    added buttons to workout with  touch response

--- workoutApp/navigation/screens/addWorkouts.js
+++ workoutApp/navigation/screens/addWorkouts.js
@@ -1,14 +1,32 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text , TouchableHighlight} from 'react-native';
 
 export default function addWorkouts({ navigation }) {
+    var [ isPress, setIsPress ] = React.useState(false);
+
+    var touchProps = {
+        activeOpacity: 1,
+        underlayColor: '#BBB',                               // <-- "backgroundColor" will be always overwritten by "underlayColor"
+        style: isPress ? styles.buttonPress : styles.button, // <-- but you can still apply other style changes
+        onHideUnderlay: () => setIsPress(false),
+        onShowUnderlay: () => setIsPress(true),
+        onPress: () => console.log(''),                 // <-- "onPress" is apparently required
+      };
+
     return (
         <View style={styles.container}>
 
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text
-                    onPress={() => navigation.navigate('Workouts')}
-                    style={{ color: 'white', fontSize: 26, fontWeight: 'bold' }}>Add workout screen</Text>
+               <TouchableHighlight {...touchProps}>
+               <Text style={{textAlign: "center", fontSize: 22}} >
+                   Add New Workout
+                </Text> 
+                </TouchableHighlight>
+                <TouchableHighlight {...touchProps}>
+                <Text style={{textAlign: "center", fontSize: 22}}>
+                   Edit Existing Workout
+                </Text>
+                </TouchableHighlight> 
             </View>
         </View>
     );
@@ -20,6 +38,26 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#5fa4ea',
       alignItems: 'center',
-      justifyContent: 'center',
+      flexDirection: "column",
+      justifyContent: 'flex-start',
+      height: "100%"
     },
+    button: {
+        backgroundColor: "white",
+        textAlign: 'center',
+        fontSize: 20,
+        padding: 20,
+        width: 300,
+        borderRadius: 5,
+        marginVertical: 10,
+    },
+    buttonPress: {
+        backgroundColor: "white",
+        textAlign: 'center',
+        fontSize: 20,
+        padding: 20,
+        width: 300,
+        borderRadius: 5,
+        marginVertical: 10,
+    }
 });
